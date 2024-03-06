3c00d18452aa3af5dcd7a9256ceb8111b46b1fe7
Roberto Payan <rpayan@acedisposal.com>
2024-03-05 17:22:28 -0700

    Se suben cambios

--- src/components/PostsItem.vue
+++ src/components/PostsItem.vue
@@ -3,6 +3,10 @@
 //Importaciones de sistemas y librerias
 import {ref, computed} from "vue";
 
+//Importaciones de componentes
+import PostModal from "./PostModal.vue";
+import PostModal2 from "./PostModal2.vue";
+
 //Importaciones de Composables
 import useFormatCurrency from "@/composables/useFormatCurrency.js";
 import useFormatDate from "@/composables/useFormatDate.js";
@@ -23,24 +27,39 @@ const props = defineProps({
 
 //Reactividad
 const urlImage = ref('');
-const totalPic = ref(0);
+const indexSelected = ref(0);
+const isModalOpened = ref(false);
 
 // Computed
 const selectImage = computed(() => {
-  urlImage.value = props.post.images[0].image_url;
+  urlImage.value = props.post.images[indexSelected.value].image_url;
   return urlImage.value;
 });
 
-
-
 //Metodos
+
+const SelectIndex = (index) => {
+  indexSelected.value = index;
+}
+
+const openModal = () => {
+  isModalOpened.value = true;
+};
+const closeModal = () => {
+  isModalOpened.value = false;
+};
+
+const submitHandler = ()=>{
+
+ console.log('thi is a test');
+  //here you do whatever
+}
 
 </script>
 
 <template>
 
-<!--                <div class="col-span-12 md:col-span-6 xxl:col-span-3">-->
-  <div class="col-span-12 md:col-span-6 xxl:col-span-2">
+  <div class="col-span-12 md:col-span-6 xxl:col-span-3">
                 <div class="box">
                   <div class="p-6">
                     <img class="w-full rounded-sm" :src="selectImage" alt="Image Description">
@@ -54,9 +73,9 @@ const selectImage = computed(() => {
                       <p class="mt-3 mb-3 text-gray-500 dark:text-white/70 text-sm">
                         {{ props.post.description }}
                       </p>
-                      <a class="p-0 font-semibold text-primary text-sm flex" href="blog-details.html">
-                        Read More<i class="ri ri-arrow-right-line ltr:ml-3 rtl:mr-3 leading-none my-auto rtl:rotate-180"></i>
-                      </a>
+                      <button @click="openModal" type="button" class="hs-dropdown-toggle ti-btn ti-border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:ring-offset-white focus:ring-primary dark:bg-bgdark dark:hover:bg-black/20 dark:border-white/10 dark:text-white/70 dark:hover:text-white dark:focus:ring-offset-white/10" data-hs-overlay="#hs-vertically-centered-scrollable-modal">
+                          Read More...
+                        </button>
                     </div>
                   </div>
                   <div class="box-footer bg-transparent">
@@ -64,23 +83,32 @@ const selectImage = computed(() => {
                     <div class="sm:flex items-center justify-between space-y-2 sm:space-y-0 xxxl:flex xxxl:space-y-0">
                       <div class="flex items-center space-x-3 rtl:space-x-reverse">
                         <div class="flex -space-x-2 rtl:space-x-reverse">
-                          <img v-for="post in props.post.images" :key="post._id" class="avatar avatar-sm ring-0 rounded-full" :src="post.image_url" alt="avatar">
+                          <img v-on:click="SelectIndex(index) " v-for="(post, index) in props.post.images" :key="post._id" class="avatar avatar-sm ring-0 rounded-full cursor-pointer" :src="post.image_url" alt="avatar">
                         </div>
                         <div>
                           <p class="text-slate-700 font-semibold text-sm text-purple-400">
-                            {{`Total Pics: ${props.post.images.length}`}}
+                            {{`(${props.post.images.length} Pics)`}}
                           </p>
 
                         </div>
                       </div>
                       <div class="">
-                       <span class="my-2">Publication Date: {{formattingDate(props.post.creationDate,'MM/DD/YYYY')}}</span>
+                       <span class="my-2">Date: {{formattingDate(props.post.creationDate,'MM/DD/YYYY')}}</span>
 
                       </div>
                     </div>
 
-
                   </div>
                 </div>
               </div>
+
+<PostModal2
+:isOpen="isModalOpened"
+@modal-close="closeModal"
+name="first-modal">
+<template #header>Custom header</template>
+</PostModal2>
+
+          
+              
 </template>
