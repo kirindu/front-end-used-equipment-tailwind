4386a1a4a8b32039e783748ec1b4966201525a16
Roberto Payan <rpayan@acedisposal.com>
2024-06-06 13:19:26 -0600

    Se iniserta la informacion de contacto en la pagina principal

--- src/components/layouts/AppPageHeader.vue
+++ src/components/layouts/AppPageHeader.vue
@@ -2,24 +2,46 @@
 </script>
 
 <template>
+    <div class="flex justify-center items-center">
+      <!-- Duplicated content horizontally -->
+      <div class="flex space-x-8">
+        <!-- First block -->
+        <div class="flex flex-col items-center" style="margin-right: 100px;">
+          <img src="/src/assets/img/logo-company/ace-logo.ico" class="h-48" alt="alternate text">
+          <div class="block justify-center page-header md:flex mt-4">
+            <!-- <div>
+              <h3 class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-white text-2xl font-medium">Used Equipment</h3>
+            </div> -->
+          </div>
+        </div>
+        <!-- Second block (duplicate) -->
+        <div class="flex flex-col items-center">
 
-
-<div class="flex justify-center items-center">
-    <img src="/src/assets/img/logo-company/ace-logo.ico" style="height: 12rem;" alt="alternate text">
-</div>
-
-        <!-- Page Header -->
-        <div class="block justify-center page-header md:flex">
+            <br>
        
             <div>
-                <h3 class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-white text-2xl font-medium"> Used Equipment</h3>
+            <h2 class="text-xl  text-gray-800 dark:text-white">
+                <span style="color: #e41a01;"  class="font-semibold mb-0 text-xl text-blue-500 leading-none">Contact:</span>
+               <p>Sam Nelson</p>
+               <p>801-836-5658</p>
+               <p>sam@caasutah.com</p>
+              </h2>
+
             </div>
-      
+
+          
+
         </div>
 
-        <!-- <div class="flex items-center justify-center">
-  <p>Text centered vertically and horizontally.</p>
-</div> -->
-        <!-- Page Header Close -->
+      
+      </div>
+    
+    </div>
 
-</template>
+  <div style="margin-bottom: 20px;">
+        <h3 class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-white text-2xl font-medium" style="text-align: center;">USED EQUIPMENT</h3>
+    </div>
+          
+
+ 
+  </template>
